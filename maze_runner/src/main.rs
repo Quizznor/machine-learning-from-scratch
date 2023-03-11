d3e021d8f33a30f2fe559fc8879a2a328b5d3fdb
Paul Filip <paul.filip@protonmail.com>
2023-03-11 23:22:04 +0100

    Add new rust project

---
+++ maze_runner/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    
+    // design goal
+    //     -> generate weighted graph using generator.rs
+    //     -> solve for shortest Path from A to B...
+    //     -> have python or something plot the result
+    
+}
