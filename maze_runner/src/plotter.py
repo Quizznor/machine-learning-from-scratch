f429b93a0eefde7089de88e150b357522059cb5b
quizznor <paul.filip@protonmail.com>
2024-06-20 16:02:54 +0000

    idek

--- maze_runner/src/plotter.py
+++ maze_runner/src/plotter.py
@@ -5,15 +5,41 @@ positions = np.load('positions.npy')
 distances = np.load('distances.npy')
 n_vertices = len(positions)
 
+def read_minimum_paths(target : str) -> list[int] :
+    
+    longest_index, longest_len = -1, 0
+
+    with open(target, 'r') as f:
+        for i, line in enumerate(f.readlines()):
+            line = [int(x) for x in line.strip().split(',')[1:]]
+            
+            if len(line) > longest_len:
+                longest_len = len(line)
+                longest_path = line
+                longest_index = i
+
+    return longest_path, longest_index
+
+longest_path, index = read_minimum_paths('minimum_distance_paths.txt')
+
 for i, ((xi, yi), edges) in enumerate(zip(positions, distances), 0):
     
-    is_start_end_point = i % (n_vertices - 1) != 0
-    plt.scatter(xi, yi, c='k' if is_start_end_point else 'r', s=20, zorder=1)
+    is_start_end_point = i in [0, index]
+    plt.scatter(xi, yi, 
+                c = 'k' if not is_start_end_point else 'r', 
+                s = 20 if not is_start_end_point else 30, 
+                zorder=1
+            )
+
     for j, (xj, yj) in enumerate(positions[i:], i):
         if distances[i, j] == 0: continue
 
         plt.plot([xi, xj], [yi, yj], lw=0.8, c='gray', alpha=0.2, zorder=0)
 
-# solution = np.load('solution.npy') # TODO
+for i in range(len(longest_path) - 1):
+    xi, yi = positions[i]
+    xf, yf = positions[i+1]
+
+    plt.plot([xi, xf], [yi, yf], c='r', lw=1.3)
 
 plt.show()
--- maze_runner/src/solver.rs
+++ maze_runner/src/solver.rs
@@ -39,29 +39,35 @@ fn dijkstra_solve(adjacency_matrix : Array2<f64>) -> Vec<String> {
     let mut paths = vec!["".to_string(); n_vertices];
     
     // needed for initialization
-    let mut next_node = 0;
+    paths[0] = "-1".to_string();
     distances[0] = 0.0;
+
+    let mut this_node = 0;
+    let mut last_node = 0;
 
     while not_visited.len() != 0 {
 
         // get connections at next node
-        let connections = adjacency_matrix.slice(s![next_node, ..]);    // get next_node connections
-        not_visited.retain(|x| *x != next_node);                        // mark next_node as visited
+        let connections = adjacency_matrix.slice(s![this_node, ..]);        // get this_node connections
+        not_visited.retain(|x| *x != this_node);                            // mark this_node as visited
 
         let mut min_distance = f64::INFINITY;
         for &n in not_visited.iter() {
 
-            if connections[n] != 0. && (distances[next_node] + connections[n]) < distances[n] {
-                distances[n] = distances[next_node] + connections[n];   // update distances
-                paths[n] = format!("{},{}", paths[n], next_node);       // save path info 
+            if connections[n] != 0. && (distances[this_node] + connections[n]) < distances[n] {
+                distances[n] = distances[this_node] + connections[n];       // update distances
+                paths[n] = format!("{},{}", paths[last_node], this_node);   // save path info 
             }
+        }
 
-            // find next_node
+        // find next node + save last node
+        last_node = this_node;
+        for &n in not_visited.iter() {
             if distances[n] < min_distance {
                 min_distance = distances[n];
-                next_node = n;
-            }     
-        }
+                this_node = n;
+            }
+        }     
     }
 
     println!("{:?}", paths);
