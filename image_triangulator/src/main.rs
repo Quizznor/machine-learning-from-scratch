a90a63da049468d5f89ea73ceab7f5f39cdbbae4
quizznor <paul.filip@protonmail.com>
2024-07-10 22:37:50 +0200

    learn a little bit about traits

--- image_triangulator/src/main.rs
+++ image_triangulator/src/main.rs
@@ -1,7 +1,7 @@
-mod optimizer;
 mod image;
 
-fn main() {
-    let source_image = image::new("example.png");
-    source_image.generate_new_mesh(100);
+pub fn main() {
+    println!("Hello World!");
+
+    let OriginalImage = image::Image::from("image.jpg");
 }
