d2ed71cc3bc4a0ea5027718e4bea81cdff3d88dc
quizznor <paul.filip@protonmail.com>
2024-07-13 15:07:26 +0000

    put image into ndarray

--- image_triangulator/src/image.rs
+++ image_triangulator/src/image.rs
@@ -1,21 +1,51 @@
 use image::open;
+use ndarray::{Array1, Array3};
+use ndarray::{s, ArrayView3};
 
 pub struct Image {
 
-    answer : i64,
+    height : u32,
+    width : u32,
+    pixels : Array3<u8>,
+}
+
+pub struct GrayScaleImage {
+    height : u32,
+    width : u32,
+    pixels : Array2<u8>,
 }
 
 impl From<&str> for Image {
     fn from(image: &str) -> Self {
         println!("works: {}", image);
 
-        let rgba = open(image).unwrap().into_rgba8();
+        let image = open(image)
+        .expect("Couldn't open image =(")
+        .into_rgb8();
 
-        println!("{}", rgba.size());        
+        let (height, width) = image.dimensions();
+        let pixels_tmp = Array1::from(
+            image.into_vec()
+        );
+
+        let pixels = pixels_tmp
+            .into_shape((height as usize, width as usize, 3))
+            .expect("Couldn't convert to ndarray =(");
 
         Image {
-            answer : 42,
+            height, 
+            width,
+            pixels,
         }
     }
 }
 
+impl Image {
+    pub fn get_sub_array(&self, x : usize, y : usize) -> ArrayView3<u8> {
+        self.pixels.slice(s![x..x+5, y..y+5, ..])
+    }
+
+    pub fn get_grayscale(&self) -> Array2<u8> {
+        
+    }
+}
--- image_triangulator/src/main.rs
+++ image_triangulator/src/main.rs
@@ -3,5 +3,9 @@ mod image;
 pub fn main() {
     println!("Hello World!");
 
-    let OriginalImage = image::Image::from("image.jpg");
+    let OriginalImage = image::Image::from("image.png");
+
+    let slice = OriginalImage.get_sub_array(0, 0);
+
+    println!("{:#?}", slice);
 }
