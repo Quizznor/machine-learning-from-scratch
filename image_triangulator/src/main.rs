b36bd4c8e7f90e476c26cf52b72aa48f8355cb4b
filip <filip@localhost.localdomain>
2025-03-05 21:59:44 +0100

    revive image triangulator

--- image_triangulator/src/main.rs
+++ image_triangulator/src/main.rs
@@ -1,56 +1,109 @@
-use clap::Parser;
+use clap::{arg, Command};
+use std::fs::canonicalize;
+use std::path::PathBuf;
 
-mod triangle;
-mod picture;
+// mod triangle;
+// mod picture;
+
 mod utl;
 
 /// Gives the triangulation of an image
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Cli {
-    /// target path of the image
-    #[arg(short, long)]
-    image: String,
+// #[derive(Parser, Debug)]
+// #[command(version, about, long_about = None)]
+// struct Cli {
+//     /// target path of the image
+//     #[arg(short, long)]
+//     image: path::Path,
 
-    /// fraction of triangles/image pixel count
-    #[arg(short, long, default_value_t = 0.003)]
-    n_tri: f64,
+//     /// fraction of triangles/image pixel count
+//     #[arg(short, long, default_value_t = 0.003)]
+//     n_tri: f64,
 
-    /// mesh generation permissivity
-    #[arg(short, long, default_value_t = 3.)]
-    power: f64,
-}
+//     /// mesh generation permissivity
+//     #[arg(short, long, default_value_t = 3.)]
+//     power: f64,
+// }
 
 pub fn main() {
-    let args = Cli::parse();
+    let matches = Command::new("ImageTriangulator")
+        .version("1.0")
+        .about("Return a low-poly representation of a given image")
+        .arg(arg!([image] "path to image").required(true))
+        // placeholders TODO
+        .arg(
+            arg!(-n --points <VALUE>)
+                .help("Number of points to use for triangulation")
+                .default_value("100")
+                .required(false)
+                .value_parser(clap::value_parser!(u32)),
+        )
+        .get_matches();
+
+    let path = canonicalize(PathBuf::from(matches.get_one::<String>("image").unwrap()))
+        .expect("Image does not exist =(");
+    let n_points = matches.get_one::<u32>("points").unwrap();
+
+    let original_image = runtime!(utl::read_image(&path), "reading image");
+    #[cfg(debug_assertions)]
+    {
+        utl::save_image(&original_image, &path, "original");
+    }
+
+    let edges = runtime!(utl::contrast(&original_image), "finding contrast");
+    #[cfg(debug_assertions)]
+    {
+        utl::save_image(&edges, &path, "edges");
+    }
+
+    let (triangulation, points) = runtime!(utl::get_triangulation(n_points, edges), "building mesh");
+    #[cfg(debug_assertions)]
+    {
+        let mesh = utl::build_mesh_image(&triangulation, &points);
+        utl::save_image(&mesh, &path, "mesh")
+    }
+    
+    // let mesh = runtime!(utl::create_mesh(coordinates), "triangulating image");
+
+    // let test = utl::image_to_array(&original_image);
+    // let test2 = utl::array_to_image(&test);
+
+    // let _ = runtime!(utl::save_image(image, "test/out_original.png"), "saving original image");
+
+    // let edges = runtime!(utl::get_edges(&image), "getting edges");
 
     // TODO!!!!!
     // better sobel edge detection
     // better mesh creation scheiss
     // fix runtime error
 
-    let mut original_image = runtime!(picture::Picture::from(&args.image), "reading image");
-    let grayscale = runtime!(original_image.to_grayscale(), "transforming to grayscale");
+    // let image = image::open(&args.image).expect("Error reading image").to_rgba8();
+    // let image: NdColor = NdImage(&image).into();
+
+    // println!("{}", type_of(&image));
+
+    // let edges = runtime!(utl::sobel(&image), "searching for object edges");
+
+    // let grayscale = runtime!(original_image.to_grayscale(), "transforming to grayscale");
 
     // determine triangulation parameters
-    let dimensions = grayscale.dim();
-    let n_triangles = (args.n_tri * dimensions.0 as f64 * dimensions.1 as f64) as usize;
+    // let dimensions = grayscale.dim();
+    // let n_triangles = (args.n_tri * dimensions.0 as f64 * dimensions.1 as f64) as usize;
 
-    #[cfg(debug_assertions)]
-    utl::save_gray_image(&grayscale, &(args.image.to_owned() + "_grayscale"));
+    // #[cfg(debug_assertions)]
+    // utl::save_gray_image(&grayscale, &(args.image.to_owned() + "_grayscale"));
 
-    let edges = runtime!(utl::sobel_operator(grayscale), "searching for object edges");
+    // utl::save_gray_image(&edges, &(args.image.to_owned() + "_grayscale"));
 
-    #[cfg(debug_assertions)]
-    utl::save_gray_image(&edges, &(args.image.to_owned() + "_edges"));
+    // #[cfg(debug_assertions)]
+    // utl::save_gray_image(&edges, &(args.image.to_owned() + "_edges"));
 
-    let mesh = runtime!(utl::create_mesh(edges, n_triangles, args.power), "building delauney mesh");
+    // let mesh = runtime!(utl::create_mesh(edges, n_triangles, args.power), "building delauney mesh");
 
-    #[cfg(debug_assertions)]
-    utl::save_mesh_image(&mesh, dimensions, &(args.image.to_owned() + "_mesh"));
+    // #[cfg(debug_assertions)]
+    // utl::save_mesh_image(&mesh, dimensions, &(args.image.to_owned() + "_mesh"));
 
-    let color_information = runtime!(utl::calculate_colors(&original_image, mesh), "sorting pixels, building triangles");
+    // let color_information = runtime!(utl::calculate_colors(&original_image, mesh), "sorting pixels, building triangles");
 
-    runtime!(original_image.apply_colormap(color_information), "apply colormap to image");
-    runtime!(original_image.save(&(args.image.to_owned() + "_triangulation")), "save triangulation picture");
+    // runtime!(original_image.apply_colormap(color_information), "apply colormap to image");
+    // runtime!(original_image.save(&(args.image.to_owned() + "_triangulation")), "save triangulation picture");
 }
--- image_triangulator/src/utl.rs
+++ image_triangulator/src/utl.rs
@@ -1,243 +1,347 @@
-use ::image::GrayImage;
-use delaunator::{triangulate, Point};
-use image::ImageFormat::Png;
-use ndarray::{array, Array1};
-use ndarray::{s, Array2};
-use plotters::prelude::*;
-use rand::{thread_rng, Rng};
+// use image::{DynamicImage, GenericImageView};
+// use image::{ImageBuffer, imageops};
 
-use crate::picture::Picture;
-use crate::triangle::Triangle;
+// use delaunator::{triangulate, Point};
+// use image::ImageFormat::Png;
+// use ndarray::{array, Array1};
+// use ndarray::{s, Array2};
+// use rand::{thread_rng, Rng};
+
+// use crate::picture::Picture;
+// use crate::triangle::Triangle;
+
+// use ndarray::prelude::*;
+// use ndarray_conv::{ConvExt::conv, ConvMode};
+
+// use ndarray_conv::{ConvExt, ConvMode};
+
+// use image::ImageError;
+// use ndarray_image::
+// use ndarray::{Array3, Array2};
 
 #[macro_export]
 macro_rules! runtime {
-    ($fctn:expr, $name:literal) => [{
-
+    ($fctn:expr, $name:literal) => {{
         let start = ::std::time::Instant::now();
         let rv = $fctn;
-        let duration = start.elapsed();
-
-        println!("{:.<40}: DONE! ({:#?})", $name, duration);
-
+        println!("{:.<40}: DONE! ({:#?})", $name, start.elapsed());
         rv
-    }];
+    }};
 }
 
-pub fn save_gray_image(target: &Array2<u8>, name: &str) -> () {
-    let (width, height) = target.dim();
-    let raw_vector = target.clone().into_raw_vec();
-
-    GrayImage::from_raw(width as u32, height as u32, raw_vector)
-        .expect("Couldn't build image from provided arguments")
-        .save_with_format("out/".to_owned() + name + ".png", Png)
-        .expect("Couldn't save image from provided arguments")
+use std::any::type_name;
+pub fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
 }
 
-pub fn save_mesh_image(
-    mesh: &Vec<Triangle>,
-    dimensions: (usize, usize),
-    name: &str,
-) -> () {
+use rand::Rng;
+use image::{open, RgbImage};
+use ndarray::{s, Array, Array1, Array2, Array3};
+use delaunator::{triangulate, Point, Triangulation};
+use ndarray_conv::{ConvExt, ConvMode, PaddingMode};
+use std::{f64::consts::PI, path::PathBuf};
 
-    let image_name = "out/".to_owned() + name + ".png";
-    let image = BitMapBackend::new(&image_name, (dimensions.0 as u32, dimensions.1 as u32))
-        .into_drawing_area();
-    image.fill(&WHITE).unwrap();
+/// read the image at <path> and return the data as [`Array3<u8>`]
+pub fn read_image(path: &PathBuf) -> Array3<u8> {
+    let image = open(path).expect("");
+    image_to_array(image.to_rgb8())
+}
 
-    let mut chart = ChartBuilder::on(&image)
-        .x_label_area_size(0)
-        .y_label_area_size(0)
-        .build_cartesian_2d(0.0..dimensions.0 as f64, 0.0..dimensions.1 as f64)
-        .unwrap();
+/// convert [`RgbImage`] to [`Array3<u8>`]
+pub fn image_to_array(image: RgbImage) -> Array3<u8> {
+    let height = image.height();
+    let width = image.width();
+    let raw = image.into_vec();
 
-    for tt in mesh.iter() {
-        chart.draw_series(LineSeries::new(tt.get_path(), &RED)).unwrap();
+    Array3::from_shape_vec((height as usize, width as usize, 3), raw).expect("")
+}
+
+/// convert [`Array3<u8>`] to [`RgbImage`]
+pub fn array_to_image(array: &Array3<u8>) -> RgbImage {
+    let (height, width, _) = array.dim();
+    let raw = array.clone().into_raw_vec_and_offset();
+
+    RgbImage::from_raw(width as u32, height as u32, raw.0).expect("")
+}
+
+/// save [`Array3<u8>`] as an image to out/[`str`]_<original_file_name>.png
+pub fn save_image(array: &Array3<u8>, path: &PathBuf, extension: &str) -> () {
+    let image = array_to_image(array);
+    let mut full_path = path.to_owned();
+
+    full_path.set_file_name(format!(
+        "out/{extension}_{}",
+        path.file_name().unwrap().to_str().unwrap()
+    ));
+    image.save(full_path).expect("")
+}
+
+/// perform rudimentary edge detection using Sobel convolution
+pub fn contrast(array: &Array3<u8>) -> Array3<u8> {
+    let array_conv = array.clone().map(|x| f64::from(*x));
+    let kernel = ndarray::array!([-1., 0., 1.], [-2., 0., 2.], [-1., 0., 1.]);
+
+    // calculate contrast over rgb channels individually
+    let (width, height, depth) = array.dim();
+    let mut contrast = Array3::<f64>::zeros((width - 2, height - 2, depth));
+    for color in [0_usize, 1_usize, 2_usize] {
+        let channel = array_conv.index_axis(ndarray::Axis(2), color).to_owned();
+        let x_edges = channel
+            .conv(&kernel, ConvMode::Valid, PaddingMode::Zeros)
+            .expect("");
+        let y_edges = channel
+            .conv(&kernel.t(), ConvMode::Valid, PaddingMode::Zeros)
+            .expect("");
+        let edges = x_edges.map(|x| x.abs()) + y_edges.map(|x| x.abs());
+        contrast.slice_mut(s![.., .., color]).assign(&edges);
     }
+
+    // MAX_PIXEL_VALUE should be 8 * 255,
+    // need to renormalize to u8 [0, 255]
+    // we devide by 4 to get some saturation
+    contrast.map(|x| (x / 4.).round() as u8).to_owned()
 }
 
-pub fn sobel_operator(gray_image: Array2<u8>) -> Array2<u8> {
-
-    let x_filter: Array2<f64> = array![
-        [-5., -4., 0., 4., 5.],
-        [-8., -10., 0., 10., 8.],
-        [-10., -20., 0., 20., 10.],
-        [-8., -10., 0., 10., 8.],
-        [-5., -4., 0., 4., 5.]
-    ];
-
-    let y_filter: Array2<f64> = array![
-        [-5., -8., -10., -8., -5.],
-        [-4., -10., -20., -10., -4.],
-        [0., 0., 0., 0., 0.],
-        [4., 10., 20., 10., 4.],
-        [5., 8., 10., 8., 5.],
-    ];
-
-    let (width, height) = gray_image.dim();
-    let mut edges = Array2::<u8>::zeros((width - 3, height - 3));
-
-    // TODO: look for a better way to do this
-    for iw in 2..width - 3 {
-        for ih in 2..height - 3 {
-            let surrounding = gray_image.slice(s![iw - 2..iw + 3, ih - 2..ih + 3]);
-            let matrix = surrounding.mapv(|x| f64::from(x));
-
-            let diff_x = (x_filter.dot(&matrix.t()) / 240.).sum();
-            let diff_y = (matrix.t().dot(&y_filter) / 240.).sum();
-
-            edges[[iw, ih]] = (diff_x.powf(2.) + diff_y.powf(2.)).powf(0.5) as u8;
-        }
-    }
-
-    edges
-}
-
-pub fn create_mesh(source: Array2<u8>, n_points: usize, power: f64) -> Vec<Triangle> {
-
-    let mut rng = thread_rng();
-    let (cdf_x, cdf_y) = get_cdf(source, power);
+/// triangulate image from randomly drawn n coordinates in a heatmap
+pub fn get_triangulation(n: &u32, edge_heatmap: Array3<u8>) -> (Triangulation, Vec<Point>) {
+    let (height, width, _) = edge_heatmap.dim();
+    let mut cdf_x = Array1::<f64>::zeros(width);
+    let mut cdf_y = Array1::<f64>::zeros(height);
     let mut points = Vec::<Point>::new();
 
-    // // chad inverse transform sampling
-    // loop {
-    //     let (x_chance, y_chance): (f64, f64) = rng.gen();
+    // bounding box 
+    let (north, south) = (height as f64 - 1., 0.);
+    let (east, west) = (width as f64 - 1., 0.);
+    points.push(Point{ x: east, y: north });
+    points.push(Point{ x: east, y: south });
+    points.push(Point{ x: west, y: north });
+    points.push(Point{ x: west, y: south });
 
-    //     let (mut min_x, mut min_y) = (f64::MAX, f64::MAX);
-    //     let (mut x, mut y): (usize, usize) = (0, 0);
+    // get piecewise cdf for x and y from grayscale image
+    {
+        let channel_coefficients = [0.2627, 0.6789, 0.0593]; // this should sum to = 1.?
+        let mut grayscale = Array2::<f64>::zeros((height, width));
 
-    //     // determine x
-    //     for (ix, x_candidate) in cdf_x.iter().enumerate() {
-    //         let dist = (x_candidate - x_chance).abs();
-    //         if dist < min_x {
-    //             min_x = dist;
-    //             x = ix;
-    //         }
-    //     }
-
-    //     // determine y
-    //     for (iy, y_candidate) in cdf_y.iter().enumerate() {
-    //         let dist = (y_candidate - y_chance).abs();
-    //         if dist < min_y {
-    //             min_y = dist;
-    //             y = iy;
-    //         }
-    //     }
-
-    //     points.push(Point {
-    //         x: x as f64,
-    //         y: y as f64
-    //     });
-
-    //     if points.len() == n_points {break;}
-    // }
-
-    
-    // virgin rejection sampling
-    loop {
-
-        // generate random x
-        let x_chance: f64 = rng.gen();
-        let mut x = 0;
-
-        loop {
-            if cdf_x[x] > x_chance {break;}
-            x += 1;
+        for (color, coeff) in channel_coefficients.iter().enumerate() {
+            let channel = edge_heatmap.index_axis(ndarray::Axis(2), color);
+            grayscale = grayscale + *coeff * channel.map(|x| f64::from(*x)) / 255.;
         }
 
-        // generate random y
-        let y_chance: f64 = rng.gen();
-        let mut y = 0;
-
-        loop {
-            if cdf_y[y] > y_chance {break;}
-            y += 1;
+        let mut running_sum = 0.;
+        for x in 0..width {
+            // println!("{x}");
+            running_sum += grayscale.column(x).sum();
+            cdf_x[x] = running_sum;
         }
-        
-        points.push(Point {
-            x: x as f64,
-            y: y as f64,
-        });
 
-        if points.len() == n_points {break;}
+        running_sum = 0.;
+        for y in 0..height {
+            // println!("{y}");
+            running_sum += grayscale.row(y).sum();
+            cdf_y[y] = running_sum;
+        }
+
+        cdf_x /= running_sum;
+        cdf_y /= running_sum;
     }
 
-    let triangulation = triangulate(&points).triangles;
-    let mut triangles = Vec::<Triangle>::new();
+    // chad inverse transform sampling
+    // x and y are independent here, which they shouldn't be
+    // how do we account for the correct prior probabilities?
+    {
+        let mut rng = rand::rng();
+        loop {
+            let (x_chance, y_chance): (f64, f64) = rng.random();
 
-    for triplet in triangulation.chunks(3).into_iter() {
-        let p1 = Point{ x: points[triplet[0]].x, y: points[triplet[0]].y };
-        let p2 = Point{ x: points[triplet[1]].x, y: points[triplet[1]].y };
-        let p3 = Point{ x: points[triplet[2]].x, y: points[triplet[2]].y };
-        triangles.push(Triangle::new(p1, p2, p3))
-    }
+            let (mut min_x, mut min_y) = (f64::MAX, f64::MAX);
+            let (mut x, mut y): (usize, usize) = (0, 0);
 
-    triangles
-}
-
-pub fn calculate_colors(
-    original_image: &Picture,
-    mesh: Vec<Triangle>
-) -> (Array2<u8>, Array2<usize>) {
-
-    let mut colors = Array2::<u64>::zeros((mesh.len(), 3));
-    let mut triangle_table = Array2::<usize>::zeros(original_image.dimension());
-    let mut triangle_pixel_counts = Array1::<usize>::ones(mesh.len());
-
-    let (width, height) = original_image.dimension();
-
-    for w in 0..width {
-        for h in 0..height {
-            for (t, triangle) in mesh.iter().enumerate() {
-                
-                if triangle.contains(Point{ x: w as f64, y: h as f64 }) {
-
-                    let pixel = original_image.get_pixel(w, h);
-                    let (r, g, b) = (pixel[[0]], pixel[[1]], pixel[[2]]);
-
-                    triangle_table[[w, h]] = t;
-                    triangle_pixel_counts[t] += 1;
-    
-                    colors[[t, 0]] += r as u64;
-                    colors[[t, 1]] += g as u64;
-                    colors[[t, 2]] += b as u64;
+            for (ix, x_candidate) in cdf_x.iter().enumerate() {
+                let dist = (x_candidate - x_chance).abs();
+                if dist < min_x {
+                    min_x = dist;
+                    x = ix;
                 }
+            }
+
+            for (iy, y_candidate) in cdf_y.iter().enumerate() {
+                let dist = (y_candidate - y_chance).abs();
+                if dist < min_y {
+                    min_y = dist;
+                    y = iy;
+                }
+            }
+
+            points.push(Point {
+                x: x as f64,
+                y: y as f64,
+            });
+
+            if points.len() == (*n) as usize {
+                break;
             }
         }
     }
 
-    for i in 0..mesh.len() {
-        colors[[i, 0]] /= triangle_pixel_counts[[i]] as u64;
-        colors[[i, 1]] /= triangle_pixel_counts[[i]] as u64;
-        colors[[i, 2]] /= triangle_pixel_counts[[i]] as u64;
-    }
-
-    (
-        colors.mapv(|x| u8::try_from(x).expect(&format!("{} encountered", x))),
-        triangle_table,
-    )
+    (triangulate(&points), points)
 }
 
-fn get_cdf(source: Array2<u8>, power: f64) -> (Array1<f64>, Array1<f64>) {
-    let weight: f64 = source.mapv(|x| f64::from(x).powf(power)).sum();
-    let array_shape = source.dim();
 
-    let mut cdf_x = Array1::<f64>::zeros(array_shape.0);
-    let mut cdf_y = Array1::<f64>::zeros(array_shape.1);
+/// get 2d image representation of a delauney triangulation
+pub fn build_mesh_image(tri: &Triangulation, points: &Vec<Point>) -> Array3<u8> {
+    let (width, height) = (points[0].x as usize + 1, points[0].y as usize + 1);
+    let mut mesh_image = Array3::<u8>::from_elem((height, width, 3), 255);
 
-    cdf_x[0] = source.column(0).mapv(|x| f64::from(x).powf(power)).sum();
-    cdf_y[0] = source.row(0).mapv(|x| f64::from(x).powf(power)).sum();
+    // // draw nodes as hollow circles
+    // for point in points {
+    //     for circum_point in _get_circumcircle(point, 1.) {
+    //         _brush(&mut mesh_image, circum_point, 2);
+    //     }
+    // }
 
-    // calculate x cdf
-    for i in 1..array_shape.0 {
-        cdf_x[i] = source.column(i).mapv(|x| f64::from(x).powf(power)).sum();
-        cdf_x[i] += cdf_x[i-1];
+    // color the points and edges of the triangulation in tri
+    for triplet in tri.triangles.chunks(3).into_iter() {
+
+        if triplet[0] < 4 || triplet[1] < 4 || triplet[2] < 4  {continue;}
+
+        let p1 = &points[triplet[0]];
+        let p2 = &points[triplet[1]];
+        let p3 = &points[triplet[2]];
+
+        for line_point in _get_line(p1, p2) {_brush(&mut mesh_image, line_point, 1);}
+        for line_point in _get_line(p2, p3) {_brush(&mut mesh_image, line_point, 1);}
+        for line_point in _get_line(p3, p1) {_brush(&mut mesh_image, line_point, 1);}
     }
 
-    // calculate x cdf
-    for i in 1..array_shape.1 {
-        cdf_y[i] = source.row(i).mapv(|x| f64::from(x).powf(power)).sum();
-        cdf_y[i] += cdf_y[i-1];
-    }
-
-    (cdf_x / weight, cdf_y / weight)
+    mesh_image
 }
+
+/// 
+
+
+/// helper function for build_mesh_image, not public
+/// return circle of points with given radius and center 
+fn _get_circumcircle(center: &Point, radius: f64) -> Vec<Point> {
+    let mut circle = vec![Point{ x: center.x + radius, y: center.y}];
+
+    for phi in Array::linspace(0., 2.* PI, 100) {
+        let new_point = Point{ 
+            x: (center.x + radius * f64::cos(phi)).round(),
+            y: (center.y + radius * f64::sin(phi)).round()
+        };
+
+        if new_point != circle[circle.len()-1] {circle.push(new_point);}
+    }
+
+    circle
+}
+
+/// helper function for build_mesh_image, not public
+/// return path of points in a straight line from A to B
+fn _get_line(a: &Point, b: &Point) -> Vec<Point> {
+    let mut line = vec![a.clone()];
+    let (x_slope, y_slope) = (a.x - b.x, a.y - b.y);
+    let d = f64::sqrt(x_slope.powi(2) + y_slope.powi(2));
+
+    for increment in 0..d as i32 {
+        let new_point = Point{
+            x: (a.x - increment as f64 * (x_slope/d)).round(),
+            y: (a.y - increment as f64 * (y_slope/d)).round()
+        };
+
+        if new_point != line[line.len()-1] {line.push(new_point);}
+    }
+
+    line
+}
+
+/// helper function for build_mesh_image, not public
+/// (maybe) fill image at a location given by point w/ 0
+fn _brush(image: &mut Array3<u8>, point: Point, brush_size: i32) -> () {
+
+    let (height, width, _) = image.dim();
+    let (x, y) = (point.x as i32, point.y as i32);
+
+    if x - brush_size < 0               // pixel under image
+    || y - brush_size < 0               // pixel over image
+    || x + brush_size >= width as i32   // pixel east of image
+    || y + brush_size >= height as i32  // pixel west of image
+    {
+        return
+    }
+
+    image.slice_mut(s![y - brush_size..y + brush_size,
+                            x - brush_size..x + brush_size, ..]).fill(0)
+}
+
+
+
+
+
+// pub fn calculate_colors(
+//     original_image: &Picture,
+//     mesh: Vec<Triangle>
+// ) -> (Array2<u8>, Array2<usize>) {
+
+//     let mut colors = Array2::<u64>::zeros((mesh.len(), 3));
+//     let mut triangle_table = Array2::<usize>::zeros(original_image.dimension());
+//     let mut triangle_pixel_counts = Array1::<usize>::ones(mesh.len());
+
+//     let (width, height) = original_image.dimension();
+
+//     for w in 0..width {
+//         for h in 0..height {
+//             for (t, triangle) in mesh.iter().enumerate() {
+
+//                 if triangle.contains(Point{ x: w as f64, y: h as f64 }) {
+
+//                     let pixel = original_image.get_pixel(w, h);
+//                     let (r, g, b) = (pixel[[0]], pixel[[1]], pixel[[2]]);
+
+//                     triangle_table[[w, h]] = t;
+//                     triangle_pixel_counts[t] += 1;
+
+//                     colors[[t, 0]] += r as u64;
+//                     colors[[t, 1]] += g as u64;
+//                     colors[[t, 2]] += b as u64;
+//                 }
+//             }
+//         }
+//     }
+
+//     for i in 0..mesh.len() {
+//         colors[[i, 0]] /= triangle_pixel_counts[[i]] as u64;
+//         colors[[i, 1]] /= triangle_pixel_counts[[i]] as u64;
+//         colors[[i, 2]] /= triangle_pixel_counts[[i]] as u64;
+//     }
+
+//     (
+//         colors.mapv(|x| u8::try_from(x).expect(&format!("{} encountered", x))),
+//         triangle_table,
+//     )
+// }
+
+// fn get_cdf(source: Array2<u8>, power: f64) -> (Array1<f64>, Array1<f64>) {
+//     let weight: f64 = source.mapv(|x| f64::from(x).powf(power)).sum();
+//     let array_shape = source.dim();
+
+//     let mut cdf_x = Array1::<f64>::zeros(array_shape.0);
+//     let mut cdf_y = Array1::<f64>::zeros(array_shape.1);
+
+//     cdf_x[0] = source.column(0).mapv(|x| f64::from(x).powf(power)).sum();
+//     cdf_y[0] = source.row(0).mapv(|x| f64::from(x).powf(power)).sum();
+
+//     // calculate x cdf
+//     for i in 1..array_shape.0 {
+//         cdf_x[i] = source.column(i).mapv(|x| f64::from(x).powf(power)).sum();
+//         cdf_x[i] += cdf_x[i-1];
+//     }
+
+//     // calculate x cdf
+//     for i in 1..array_shape.1 {
+//         cdf_y[i] = source.row(i).mapv(|x| f64::from(x).powf(power)).sum();
+//         cdf_y[i] += cdf_y[i-1];
+//     }
+
+//     (cdf_x / weight, cdf_y / weight)
+// }
